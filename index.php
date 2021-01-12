5f2de6e56663041cdf8fdb7d7f929111dbcd09a3
Lincoln Williams <git@linconwilliams.com>
2021-01-12 16:01:39 -0500

    Working on Welcome and Consent Page; Still Unstable

--- index.php
+++ index.php
@@ -1,13 +1,27 @@
 <?php
 
+if ( isset($_COOKIE['consent']) and $_COOKIE['consent'] == 'yes' ) {
+    echo('<h1>classi</h1>');
+    header("Refresh:2");
+}
+
+ob_start();
+
 echo('
 
 <center>
 
 <h1>Hello</h1>
 <p>By clicking the link below and continuing to classi, you acknowledge that classi is beta software and consent to the use of browser cookies and/or other types of tracking for functional purposes and/or improving the classi service. No information will ever be sold to third parties.</p>
-<h3><a href="assignments.php">Continue to classi</a></h3>
+<h3><a href="?consent=true">OK</a></h3>
 
 </center>
 
 ');
+
+if ( isset($_GET['consent']) and $_GET['consent'] == 'true' ) {
+    echo('Thank you! You will be redirected to classi in a few moments.');
+    setcookie('consent', 'yes', time() + (86400 * 30 * 9999));
+}
+
+ob_end_flush();
