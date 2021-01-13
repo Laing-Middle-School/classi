d3afe3aba291a01f9c426e4619fd2f5fe484709f
Lincoln Williams <git@linconwilliams.com>
2021-01-13 12:52:19 -0500

    Fixing Error with Login Cookies

--- assignments.php
+++ assignments.php
@@ -112,6 +112,37 @@ $client->addScope(Google_Service_Classroom::CLASSROOM_COURSEWORK_ME);
 $client->setLoginHint($_COOKIE['auth-login-hint']);
 
 if (isset($_SESSION['access_token'])) {
+
+  $payload = $client->verifyIdToken($id_token);
+  if ($payload) {
+    $userid = $payload['sub'];
+
+    // If request specified a G Suite domain:
+    $domain = $payload['hd'];
+  } else {
+    echo('
+      <!-- Page Content -->
+      <div class="container">
+
+        <!-- Heading Row -->
+        <div class="row align-items-center my-5">
+          <div class="col-lg-7">
+            <img class="img-fluid rounded mb-4 mb-lg-0" src="https://raw.githubusercontent.com/Laing-Middle-School/classi/master/classi.png" alt="">
+          </div>
+          <!-- /.col-lg-8 -->
+          <div class="col-lg-5">
+            <h1 class="font-weight-light">Shouldn'."'".'t you be in class right now?</h1>
+            <p>Welcome to classi! If you have an account please click the button below to login.</p>
+            <a class="btn btn-primary" href="' . $c->get()['important']['install-loc'] . '/vault.php">Log In</a>
+          </div>
+          <!-- /.col-md-4 -->
+        </div>
+        <!-- /.row -->
+    ');
+
+    exit;
+  }
+
   $client->setAccessToken($_SESSION['access_token']);
 
   $google_oauth = new Google_Service_Oauth2($client);
