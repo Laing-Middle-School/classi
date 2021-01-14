8262e44175a0cab52d3a45f4547f8c5136a34fd5
Lincoln Williams <git@linconwilliams.com>
2021-01-14 13:11:23 -0500

    Fixed Communications Pages (Corrected Navbars and Added Protection Against Non-Authenticated Users)

--- auth-protect.php
+++ auth-protect.php
@@ -14,30 +14,9 @@ $client->setLoginHint($_COOKIE['auth-login-hint']);
 
 
 function kickToLogin() {
-    echo('
-      <!-- Page Content -->
-      <div class="container">
-
-        <!-- Heading Row -->
-        <div class="row align-items-center my-5">
-          <div class="col-lg-7">
-            <img class="img-fluid rounded mb-4 mb-lg-0" src="https://raw.githubusercontent.com/Laing-Middle-School/classi/master/classi.png" alt="">
-          </div>
-          <!-- /.col-lg-8 -->
-          <div class="col-lg-5">
-            <h1 class="font-weight-light">Shouldn'."'".'t you be in class right now?</h1>
-            <p>Welcome to classi! If you have an account please click the button below to login.</p>
-            <a class="btn btn-primary" href="vault.php">Log In</a>
-          </div>
-          <!-- /.col-md-4 -->
-        </div>
-        <!-- /.row -->
-    ');
-
+    header('Location: index.php');
     exit;
 }
-
-
 
 
 
