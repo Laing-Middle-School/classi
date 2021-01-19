69afb502b1a23c9c7ca2fefa661367ed7f5fe621
Lincoln Williams <git@linconwilliams.com>
2021-01-19 10:58:07 -0500

    Added Canvas Beta (for Testing Only)

--- canvas-beta.php
+++ canvas-beta.php
@@ -8,18 +8,25 @@ $client = new Client();
 
 
 
-// FYI this token isn't connected
-$token = "11531~pSCgAK3OTlHhLbusvyOeMdLGAiZTB31640OtS2ZvWseX4HrdYEXXBxMvYPqbSyL5";
+// You must put your own token here
+$token = "YOUR_TOKEN_HERE";
 
 $response = json_decode($client->get('https://canvas.instructure.com/api/v1/courses?access_token=' . $token)->getBody());
 
 foreach ( $response as $item ) {
     echo($item->name . '<br><br>');
 
-    $assignments = json_decode($client->get('https://canvas.instructure.com/api/v1/courses/' . $item->id . '/assignments?access_token=' . $token)->getBody());
+    $assignments = json_decode($client->get('https://canvas.instructure.com/api/v1/courses/' . $item->id . '/assignments?bucket=upcoming&access_token=' . $token)->getBody());
+
+    $i = 0;
 
     foreach ( $assignments as $assignment ) {
         echo($assignment->name . '<br>');
+        $i++;
+    }
+
+    if ( $i == 0 ) {
+        echo('<b>No Assignments</b>');
     }
 
     echo('<br><br><br>');
