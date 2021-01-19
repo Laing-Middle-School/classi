8709a74822c3c4709fce8dc08e6c4a787101ee77
Lincoln Williams <git@linconwilliams.com>
2021-01-19 09:31:54 -0500

    Added Canvas Beta (for Testing Only)

---
+++ canvas-beta.php
@@ -0,0 +1,9 @@
+<?php
+
+require('vendor/autoload.php');
+
+$token = "11531~pSCgAK3OTlHhLbusvyOeMdLGAiZTB31640OtS2ZvWseX4HrdYEXXBxMvYPqbSyL5";
+
+$response = $client->get('https://canvas.instructure.com/api/v1/courses?access_token=' . $token)->getBody();
+
+echo $response;
