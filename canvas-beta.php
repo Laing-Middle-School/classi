e2249537e83b9d1557bfd0c4111bdd18466fa1dd
Lincoln Williams <git@linconwilliams.com>
2021-01-19 09:58:43 -0500

    Added Canvas Beta (for Testing Only)

--- canvas-beta.php
+++ canvas-beta.php
@@ -14,7 +14,15 @@ $token = "11531~pSCgAK3OTlHhLbusvyOeMdLGAiZTB31640OtS2ZvWseX4HrdYEXXBxMvYPqbSyL5
 $response = json_decode($client->get('https://canvas.instructure.com/api/v1/courses?access_token=' . $token)->getBody());
 
 foreach ( $response as $item ) {
-    echo $item->name;
+    echo($item->name . '<br><br>');
+
+    $assignments = json_decode($client->get('https://canvas.instructure.com/api/v1/courses/' . $item->id . '/assignments?access_token=' . $token)->getBody());
+
+    foreach ( $assignments as $assignment ) {
+        echo($assignment->name . '<br>');
+    }
+
+    echo('<br><br><br>');
 }
 
 // echo $response;
