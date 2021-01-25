ec3793c5fb9a6576ef5377cfe5b32de443b39562
lincolnthedev <lincolnthedev@askthetech.guru>
2021-01-25 09:11:41 -0500

    Added More Options in Super Link

--- super-link.php
+++ super-link.php
@@ -46,6 +46,63 @@ if ( $_GET['location'] == 'achieve3000' ) {
     ');
 }
 
+if ( $_GET['location'] == 'mcgrawhill' ) {
+    echo('
+        <title>classi</title>
+        <center><br>
+            <img src="resources/preloader.gif"/>
+            <h1 style="font-size:70px"><b>classi</b> Super Link</h1>
+            <p>By <b>@lincolnthedev</b></p>
+            <script>
+                sl_popup = window.open("https://clever.com/oauth/google/login");
+                setTimeout(closepopup, 1000);
+                function closepopup() {
+                    sl_popup.close();
+                }
+            </script>
+            <meta http-equiv="refresh" content="3;url=https://clever.com/oauth/google/login?target=NTQ2NjA4ZTJhMmNiNGMwODEwMDAwNTk5;NThhMjM4Nzg2MGNjMDllN2QxZDE=;aHR0cHM6Ly9sb2dpbi5taGNhbXB1cy5jb20vU1NPL0RJL0NsZXZlci9MVElIYW5kbGVyLmFzaHg/dG9vbF9pZD1jb25uZWN0ZWQ=;;Y29kZQ==;">
+        </center>
+    ');
+}
+
+if ( $_GET['location'] == 'noredink' ) {
+    echo('
+        <title>classi</title>
+        <center><br>
+            <img src="resources/preloader.gif"/>
+            <h1 style="font-size:70px"><b>classi</b> Super Link</h1>
+            <p>By <b>@lincolnthedev</b></p>
+            <script>
+                sl_popup = window.open("https://clever.com/oauth/google/login");
+                setTimeout(closepopup, 1000);
+                function closepopup() {
+                    sl_popup.close();
+                }
+            </script>
+            <meta http-equiv="refresh" content="3;url=https://clever.com/oauth/google/login?target=NTQ2NjA4ZTJhMmNiNGMwODEwMDAwNTk5;ODMxYWQwNTQxOGFhMTE0ZGVhMmE=;aHR0cHM6Ly93d3cubm9yZWRpbmsuY29tL2F1dGgvY2xldmVyL2NhbGxiYWNr;;Y29kZQ==;">
+        </center>
+    ');
+}
+
+if ( $_GET['location'] == 'noredink' ) {
+    echo('
+        <title>classi</title>
+        <center><br>
+            <img src="resources/preloader.gif"/>
+            <h1 style="font-size:70px"><b>classi</b> Super Link</h1>
+            <p>By <b>@lincolnthedev</b></p>
+            <script>
+                sl_popup = window.open("https://clever.com/oauth/google/login");
+                setTimeout(closepopup, 1000);
+                function closepopup() {
+                    sl_popup.close();
+                }
+            </script>
+            <meta http-equiv="refresh" content="3;url=https://clever.com/oauth/google/login?target=NTQ2NjA4ZTJhMmNiNGMwODEwMDAwNTk5;OGJmNjg1NTI5YTU1NWZlNmJiMmU=;aHR0cHM6Ly9hcGkuYnJhaW5wb3AuY29tL2FwaS9jbGV2ZXJjYWxsYmFjay9icC9lbg==;;Y29kZQ==;">
+        </center>
+    ');
+}
+
 
 
 
